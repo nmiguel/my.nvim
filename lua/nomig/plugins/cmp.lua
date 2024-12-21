d23ab06bca952a5f9bfffd178aa3065907ad6be5
Nuno Ramos <nuno.m.ramos@tecnico.ulisboa.pt>
2024-12-21 22:27:34 +0000

    extra settings for blink.cmp

--- lua/nomig/plugins/cmp.lua
+++ lua/nomig/plugins/cmp.lua
@@ -5,8 +5,7 @@ return {
 	version = "v0.*",
 
 	config = function()
-		---@diagnostic disable: missing-fields
-        local rounded_border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" }
+		local rounded_border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" }
 		require("blink.cmp").setup({
 			appearance = {
 				nerd_font_variant = "normal",
@@ -30,17 +29,20 @@ return {
 
 				documentation = {
 					auto_show = true,
+					auto_show_delay_ms = 100,
 					window = {
 						border = rounded_border,
 					},
 				},
-
-				signature = {
-					enabled = true,
-					window = {
-						border = rounded_border,
-					},
+			},
+			signature = {
+				enabled = true,
+				window = {
+					border = rounded_border,
 				},
+			},
+			sources = {
+				default = { "lsp", "path", "buffer" },
 			},
 		})
 	end,
